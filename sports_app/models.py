2fc6ed66782f654387537c925326a0a5e8690d20
MorsImmortalis05 <morsimmortalis05@gmail.com>
2025-08-21 22:29:30 +0300

    created workout routine model and made models more logical

--- sports_app/models.py
+++ sports_app/models.py
@@ -1,62 +1,8 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
-class User(AbstractUser):
-
-    def __str__(self):
-        return f"{self.username} ({self.first_name} {self.last_name})"
-
-
-class Exercise(models.Model):
-    MUSCLE_GROUP_CHOICES = [
-        ("chest", "Chest"),
-        ("back", "Back"),
-        ("shoulders", "Shoulders"),
-        ("arms", "Arms"),
-        ("core", "Core"),
-        ("legs", "Legs"),
-    ]
-
-    image = models.ImageField()
-    muscles_group = models.CharField(
-        max_length=10,
-        choices=MUSCLE_GROUP_CHOICES
-    )
-    description = models.TextField()
-
-
-class Workout(models.Model):
-
-    PROGRAM_TYPE_CHOICES = [
-        ("Upper body", "Workout for upper body"),
-        ("Legs day", "Workout for legs"),
-        ("Core", "Workout for core muscles"),
-        ("Yoga", "Yoga workout"),
-        ("Pilates", "Pilates"),
-        ("Stretching", "Stretching")
-    ]
-
-
-    name = models.CharField(max_length=255)
-    author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="my_workouts"
-    )
-    saved_by = models.ManyToManyField(
-        User,
-        related_name="saved_workouts"
-        )
-    exercises = models.ManyToManyField(
-        Exercise,
-        related_name="+"
-    )
-    type_of_program = models.CharField(
-        max_length=25,
-        choices=PROGRAM_TYPE_CHOICES
-    )
-    time = models.DurationField()
+User = get_user_model()
 
 
 class Profile(models.Model):
@@ -106,9 +52,74 @@ class Profile(models.Model):
         choices=ACTIVITY_LEVEL_CHOICES,
         default="sedentary"
     )
-    saved_workouts = models.ManyToManyField(
-        Workout,
-        related_name="saved_by")
 
     def __str__(self):
         return f"{self.user.username}"
+
+
+class Exercise(models.Model):
+    MUSCLE_GROUP_CHOICES = [
+        ("chest", "Chest"),
+        ("back", "Back"),
+        ("shoulders", "Shoulders"),
+        ("arms", "Arms"),
+        ("core", "Core"),
+        ("legs", "Legs"),
+    ]
+
+    image = models.ImageField()
+    muscles_group = models.CharField(
+        max_length=10,
+        choices=MUSCLE_GROUP_CHOICES
+    )
+    description = models.TextField()
+
+
+class Workout(models.Model):
+
+    PROGRAM_TYPE_CHOICES = [
+        ("Upper body", "Workout for upper body"),
+        ("Legs day", "Workout for legs"),
+        ("Core", "Workout for core muscles"),
+        ("Yoga", "Yoga workout"),
+        ("Pilates", "Pilates"),
+        ("Stretching", "Stretching")
+    ]
+
+
+    name = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="my_workouts"
+    )
+    saved_by = models.ManyToManyField(
+        Profile,
+        related_name="saved_workouts"
+        )
+    exercises = models.ManyToManyField(
+        Exercise,
+        related_name="exercises"
+    )
+    type_of_program = models.CharField(
+        max_length=25,
+        choices=PROGRAM_TYPE_CHOICES
+    )
+    time = models.DurationField()
+
+
+class WorkoutRoutine(models.Model):
+    profile = models.ForeignKey(
+        Profile,
+        on_delete=models.CASCADE,
+        related_name="routines"
+    )
+    workout = models.OneToOneField(
+        Workout,
+        on_delete=models.SET_NULL,
+        related_name="routine_workout",
+        null=True
+    )
+    time = models.DurationField(null=True, blank=True)
+    datetime = models.DateTimeField(null=True, blank=True)
+    notes = models.TextField(null=True, blank=True)
